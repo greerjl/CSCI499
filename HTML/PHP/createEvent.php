0de14cbb3b5738b3248da43d26ee1f47e6d8b5d3
Caleb LaVergne <lavergcd@plu.edu>
2017-03-31 15:24:55 -0700

    Work done on php files
    Created several new php files for chores, tasks, and events that will process and clean input. PHP to create new chores, tasks, and events also added. Started writing code to autofill dropboxes with room names and usernames in order to make filling out online forms easier when a user is creating chores, tasks, or event. All files likely still need work as far as integration, however all have been checked via php checker for syntax errors.

---
+++ HTML/PHP/createEvent.php
@@ -0,0 +1,27 @@
+<?php
+	include '../dbconnect.php';
+	//$eTitle = $eDesc = $roomName = $username = $sql = "";
+	
+	if($_SERVER['REQUEST_METHOD']=='POST' && $_POST){
+			$sql = "SELECT RID FROM room WHERE name = '$roomName' AND GID = '$gid'";
+			$result = mysqli_query($db, $sql);
+			
+			$count = mysqli_num_rows($result);
+			if($count != 1){
+				die('Error: ' . mysqli_error());			
+			}
+			else{
+				$rid = mysqli_fetch_row($result);
+				$rid = $rid[0];
+				$sql = "INSERT INTO event (RID, name, description, GID) VALUES ('$rid','$eTitle','$eDesc','$gid')";
+				$result = mysqli_query($db, $sql);
+				
+				if(!$result){
+					die('Error: ' . mysqli_error());				
+				}
+				else{
+					echo "Event successfully created!";			
+				}			
+			}
+	}
+?>
