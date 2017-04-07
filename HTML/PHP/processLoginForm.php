74d2c5625a0960dcdf5666f236cf2cf69a60dcab
Gage <gibsongd@plu.edu>
2017-04-07 16:55:30 -0700

    restructure of login in processLogin
    moved lines around, also password_verify() was using the wrong parameters in the wrong order haha

--- HTML/PHP/processLoginForm.php
+++ HTML/PHP/processLoginForm.php
@@ -15,26 +15,25 @@
 		$mypassword = mysqli_real_escape_string($db, $_POST['pswd']);
 		cleanData($mypassword);
 
-		//Password hash functions
-		$hash = password_hash($mypassword, PASSWORD_DEFAULT)."\n";
-
 		//Database queries
-		$sql = "SELECT UID FROM user_info WHERE username = '$myusername' and password = '$hash'";
-		$sqlPswd = "SELECT password FROM user_info WHERE username= '$myusername'";
-		$pwsdResult = mysqli_query($db, $sqlPswd);
-
-		$result = mysqli_query($db, $sql);
-		//** line not necessary $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-		$count = mysqli_num_rows($result);
+		$sqlPswd = "SELECT password FROM user_info WHERE username= '$myusername'";//hashed password
+		$pswdResult = mysqli_query($db, $sqlPswd);
 
 		//if statement to allow login and start session if account exists and password is correct
-		if (password_verify($pwsdResult, $hash) && $count == 1) {
+		if (password_verify($mypassword, $pswdResult)) {
+			echo "after pw verify in processLogin";
+			$sql = "SELECT UID FROM user_info WHERE username = '$myusername'";
+			$result = mysqli_query($db, $sql);
+			//** line not necessary $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+			$count = mysqli_num_rows($result);
+			if($count == 1){
     			session_start();
 					$_SESSION["login_user"] = $myusername;
 	    			$_SESSION["valid"] = true;
 	    			$_SESSION["timeout"] = time() + 300;
 
 					redirect("../welcome.php");
+			}//if
 		} else {
 			$hasErrors = true;
 			redirect("../login.php");
