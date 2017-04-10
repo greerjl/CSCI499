2d3acedb9cf814d85301136e5615fe1a498fa4d8
Gage <gibsongd@plu.edu>
2017-04-10 14:59:33 -0700

    Connecting createRoom php and html
    connected createRoom with the webpage form for the purpose of adding rooms to a groups house. Ready for testing.

--- HTML/PHP/createRoom.php
+++ HTML/PHP/createRoom.php
@@ -1,28 +1,35 @@
 <?php
-	include '../../../dbconnect.php';
+	require_once('../../../dbconnect.php');
 
 	if($_SERVER['REQUEST_METHOD']=='POST' && $_POST){
-		
-		//room attributes sent from creation page
-		$rName = mysqli_real_escape_string($db, $_POST['roomName']);
-		cleanData($rName);
-		$rDesc = mysqli_real_escape_string($db, $_POST['description']);
-		cleanData($rDesc);
-		$rSize = mysqli_real_escape_string($db, $_POST['roomSize']);
-		cleanData($rSize);
-		
-		global $gid;
-		$sql = "INSERT INTO room (name, description, size, GID) VALUES ('$rName','$rDesc','$rSize','$gid')";
-		$result = mysqli_query($db, $sql);
 
-		if(!$result){
-			die('Error: ' . mysqli_error());
-		}//if
-		else{
-			echo "Room successfully created!";
-		}//else
+		//room attributes sent from creation page
+		$rName1 = mysqli_real_escape_string($db, $_POST['roomName']);
+		cleanData($rName1);
+		$rName2 = mysqli_real_escape_string($db, $_POST['roomName']);
+		cleanData($rName2);
+		$rName3 = mysqli_real_escape_string($db, $_POST['roomName']);
+		cleanData($rName3);
+		$rName4 = mysqli_real_escape_string($db, $_POST['roomName']);
+		cleanData($rName4);
+
+		$gid = 101;
+		$roomArr = {$rName1, $rName2,$rName3,$rName4};
+		foreach($roomArr as &$room){
+			$sql = "INSERT INTO room (name, description, size, GID) VALUES ('$room','$gid')";
+			$result = mysqli_query($db, $sql);
+
+			if(!$result){
+				die('Error: ' . mysqli_error());
+			}//if
+			else{
+				echo "Room successfully created!";
+			}//else
+		}
+		unset($room);
+
 	}//if
-	
+
 	function cleanData($data){
 		$data = trim($data);
 		$data = stripslashes($data);
