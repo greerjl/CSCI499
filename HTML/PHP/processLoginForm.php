ebb22cadbddf8932afd4b2618506061ad011c5e2
Caleb LaVergne <lavergcd@plu.edu>
2017-04-11 18:06:27 -0700

    Commented out all echo statements
    echo statements commented out to ensure that redirect will work.

--- HTML/PHP/processLoginForm.php
+++ HTML/PHP/processLoginForm.php
@@ -7,28 +7,28 @@
 
 	include "/home/capstone/Desktop/DocRoot/dbconnect.php";
 
-echo "bitch."."\n";
+//echo "bitch."."\n";
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 		//username and password sent from form
 		$myusername = mysqli_real_escape_string($db, $_POST['usnm']);
-		echo $myusername."\n";
+		//echo $myusername."\n";
 		cleanData($myusername);
 		$mypassword = mysqli_real_escape_string($db, $_POST['pswd']);
-		echo $mypassword."\n";
+		//echo $mypassword."\n";
 		cleanData($mypassword);
 
 		//Database queries
 		$sqlPswd = "SELECT password FROM user_info WHERE username= '$myusername'";//grab stored hashed password
-		echo "sqlPswd = ".$sqlPswd."\n";
+		//echo "sqlPswd = ".$sqlPswd."\n";
 		$pswdResult = mysqli_query($db, $sqlPswd);
 
-		echo "myusername = ".$myusername."\n";
+		//echo "myusername = ".$myusername."\n";
 		//echo "hashed pword = ".$pswdResult[0]."\n";
 
 		//if statement to allow login and start session if account exists and password is correct
 		if (password_verify($mypassword, $pswdResult)) {
-			echo "after pw verify in processLogin";
+			//echo "after pw verify in processLogin";
 			$sql = "SELECT UID FROM user_info WHERE username = '$myusername'";
 			$result = mysqli_query($db, $sql);
 			//** line not necessary $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
