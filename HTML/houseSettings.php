34162aa0755cc5494cab8a89f9307a15b19286d3
Gage <gibsongd@plu.edu>
2017-04-11 18:05:33 -0700

    fixed css
    spacing issues with form titles

--- HTML/houseSettings.php
+++ HTML/houseSettings.php
@@ -15,7 +15,7 @@
     <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <!--<link href="../bootstrap/css/4-col-portfolio.css" rel="stylesheet">-->
+    <link href="../bootstrap/css/4-col-portfolio.css" rel="stylesheet">
 
     <!-- room form css -->
     <link href="../CSS/roomForm.css" rel="stylesheet">
