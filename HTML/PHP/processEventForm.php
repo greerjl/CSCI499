ac49bb332059c1d15d2993ff7e9824cd307a9d10
Caleb LaVergne <lavergcd@plu.edu>
2017-04-13 17:16:08 -0700

    Edit processEventForm to include createEventForm code
    Code simply moved. Needs to be edited in order to properly work (reference processChoreForm)

--- HTML/PHP/processEventForm.php
+++ HTML/PHP/processEventForm.php
@@ -92,4 +92,28 @@ error_reporting(E_ALL);
 			}//case roomName
 		}
 	}
-?>
+	
+	function send(){
+			if($_SERVER['REQUEST_METHOD']=='POST' && $_POST){
+				$sql = "SELECT RID FROM room WHERE name = '$roomName' AND GID = '$gid'";
+				$result = mysqli_query($db, $sql);
+	
+				$count = mysqli_num_rows($result);
+				if($count != 1){
+					die('Error: ' . mysqli_error());
+				}
+				else{
+					$rid = mysqli_fetch_row($result);
+					$sql = "INSERT INTO event (RID, name, description, GID) VALUES ('$rid','$eTitle',$gid)";
+					$result = mysqli_query($db, $sql);
+	
+					if(!$result){
+						die('Error: ' . mysqli_error());
+					}
+					else{
+						echo "Event successfully created!";
+					}
+				}
+		}	
+	}
+?>	
