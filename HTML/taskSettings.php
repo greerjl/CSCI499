61fdcabaaf7536c5112f763dd6518e1a339b1092
Gage <gibsongd@plu.edu>
2017-04-17 15:39:50 -0700

    continued work in appearance of profile pages
    Dashboard (profile.php), houseSettings, choreSettings, taskSettings, and eventSettings

--- HTML/taskSettings.php
+++ HTML/taskSettings.php
@@ -21,7 +21,7 @@
 
     <link rel="stylesheet" type="text/css" href="../CSS/normalize.css"/> <!-- normalize -->
     <link rel="stylesheet" type="text/css" href="../CSS/psuedoWelcome.css"/> <!-- css -->
-    
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -50,7 +50,7 @@
     </div>
 
     <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <nav class="navbar navbar-inverse navbar-fixed-top2" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
@@ -61,11 +61,12 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="./welcome.php">Profile</a>
+                <a class="navbar-brand" href="./welcome.php">Dashboard</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
+
                     <li>
                         <a href="./houseSettings.php">House</a>
                     </li>
@@ -77,6 +78,12 @@
                     </li>
                     <li>
                         <a href="./eventSettings.php">Events</a>
+                    </li>
+                    <li>
+                        <a href="#"> My Settings </a>
+                    </li>
+                    <li>
+                        <a href="./logout.php"> Logout </a>
                     </li>
                 </ul>
             </div>
