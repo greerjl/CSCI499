6d0d19fb6a730a21acde13acdc223b828d9d529f
Caleb LaVergne <lavergcd@plu.edu>
2017-04-18 16:29:19 -0700

    Edit to dbCheck logic
    Logic was backwards for checking db for if an email had already been used. fixed

--- HTML/PHP/processSignupForm.php
+++ HTML/PHP/processSignupForm.php
@@ -6,6 +6,7 @@
 	$email = $pswd = $rpswd = "";
 	$sql = ""; $hash = "";
 	$emailErr = $pswdErr = $rpswdErr = $dbErr = "";
+	$db = $GLOBALS['db'];
 	$hasErrors = false;
 
 	if($_SERVER['REQUEST_METHOD']=='POST' && $_POST){
@@ -13,9 +14,9 @@
 		/*USER CREDENTIALS*/
 		$email = cleanData($_POST['email']);
 			$emailErr = validate($email, 'email');
-			if(!empty($emailErr)) $hasErrors = true;
+			if(!empty($emailErr)){ $hasErrors = true;}//if
 			$dbErr = dbCheck($email, 'email');
-			if(!empty($dbErr)) $hasErrors = true;
+			if(!empty($dbErr)){ $hasErrors = true;}//if
 
 		$pswd = cleanData($_POST['pswd']);
 			$pswdErr = validate($pswd, 'password');
@@ -23,7 +24,6 @@
 			else{
 					//hashing function ** need to make sure this is being saved to DB
 					$hash = password_hash($pswd, PASSWORD_BCRYPT);
-					//echo "hash = ".$hash;
 			}//else
 
 		$rpswd = cleanData($_POST['rpswd']);
@@ -122,15 +122,15 @@
 	function dbCheck($data, $field){
 		switch($field){
 			case 'email': {
-				$sql = "SELECT email FROM user_info WHERE email = $data";	
+				$sql = "SELECT email FROM user_info WHERE email = '$data'";	
 				
-				$result = mysqli_query($GLOBALS['db'], $sql);
+				$result = mysqli_query($db, $sql);
 				$count = mysqli_num_rows($result);
 				
-				if($count == 0){
+				if($count == 1){
 					return "This email has already been registered.";
 				}//if
-				elseif($count == 1){
+				elseif($count == 0){
 					return "";
 				}//elseif
 				else{
