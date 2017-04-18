eff2db80eae8e22f2498686811ada35dfe94a293
greerjl <greerjl@plu.edu>
2017-04-17 17:51:59 -0700

    revert

--- HTML/houseSettings.php
+++ HTML/houseSettings.php
@@ -51,10 +51,10 @@
       </div>
 
     <!-- Navigation -->
-    <nav class="navbar navbar-inverse fixed-top" role="navigation">
+    <nav class="navbar navbar-inverse" role="navigation">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header navbar-brand">
+            <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
