d3cb1a7771f3334d6b6639f91d29d441aeca5c9d
Gage <gibsongd@plu.edu>
2017-04-18 17:32:01 -0700

    Home to House
    because we are House Utilities Manager, mf's

--- HTML/welcome.php
+++ HTML/welcome.php
@@ -40,7 +40,7 @@
 
     <div id="main">
         <div class="header">
-            <h1>Home Utilities Manager</h1>
+            <h1>House Utilities Manager</h1>
             <h2>An application housing all your home management needs. </h2>
         </div>
 
