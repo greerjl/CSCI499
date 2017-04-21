2b8b511d2018919173e619d35819ded7b03cb689
greerjl <greerjl@plu.edu>
2017-04-20 17:12:06 -0700

    Rearranged email code

--- HTML/PHP/phpmailer.php
+++ HTML/PHP/phpmailer.php
@@ -8,36 +8,13 @@ $mail->setFrom('HouseUtilitiesManager@gmail.com', 'HUM');
 $mail->addAddress($email, $username);
 $mail->Subject  = 'Welcome to HUM!';
 $mail->isHTML(true);
-$mail->Body = '
-
-  <head>
-    <title>Welcome</title>
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <style>
-
-    </style>
-  </head>
-  <body>
-    <div class="header">
-        <h1>House Utilities Manager</h1>
-        <h2>An application for all your house management needs. </h2>
-    </div><!-- header -->
-    <div class="content">
-    <h4>Hi '.$username.', </h4>
-    <p> Thanks for signing up for House Utilities Manager. We are very
-        excited to have you on board.
-    </p>
-    <p> To get started using HUM, please confirm your account below: </p>
-    <br>
-    <form action="http://www.houseutil.com/HTML/verified.php?email='.$email.'&hash='.$accesskey'">
-      <button class="btn btn-lg btn-primary btn-block" type="submit">
-          Confirm your account</button>
-    </form>
-    <p> Thanks, <br> The HUM Team </p>
-  </body>
-
-';
+$mail->Body = '<html><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
+$mail->Body = '</head><body><div class="header"><h1>House Utilities Manager</h1>';
+$mail->Body = '<h2>An application for all your house management needs. </h2></div>';
+$mail->Body = '<div class="content"><h4>Hello, </h4><p> Thanks for signing up for House Utilities Manager. We are very excited to have you on board.</p>';
+$mail->Body = '<p> To get started using HUM, please confirm your account below: </p><br>';
+$mail->Body = '<form action="http://www.houseutil.com/HTML/verified.php?email='.$email.'&hash='.$accesskey'"><button class="btn btn-lg btn-primary btn-block" type="submit">Confirm your account</button></form>';
+$mail->Body = 'Thanks, <br> The HUM Team </p></body></html>';
 $mail->AltBody = "Hi <?php echo $username; ?>, Thanks for signing up for House Utilities Manager. We are very
     excited to have you on board! To get started using HUM, please confirm your account below:";
 
