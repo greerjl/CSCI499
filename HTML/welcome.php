e71956b0fdf74f365b51004dcbab0664cc3764dc
Gage <gibsongd@plu.edu>
2017-04-26 17:52:21 -0700

    changed class name for completed button
    and created new block in roomForm.css

--- HTML/welcome.php
+++ HTML/welcome.php
@@ -140,7 +140,7 @@ if($_SESSION["valid"]==true){?>
                       while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
                           foreach ($line as $col_value) {
                               echo "\t\t<tr><td><strong>$col_value</strong></td>
-                              <td><form action=\"./PHP/choreCompleted.php\"><input class=\"txt2\" type=\"submit\"
+                              <td><form action=\"./PHP/choreCompleted.php\"><input class=\"txt3\" type=\"submit\"
                                 style=\"border-radius: 6px;\" type=\"submit\" value=\"Completed\"/></form></td></tr>";
                           }//foreach
                       }//while
@@ -170,7 +170,7 @@ if($_SESSION["valid"]==true){?>
                                 $name = $line['name'];
                                 $time = $line['time'];
                                 echo "\t\t<tr><td><strong>$name</strong></td><td>---</td><td> by $time</td><td>---</td>
-                                <td><form action=\"./PHP/taskCompleted.php\"><input class=\"txt2\" type=\"submit\"
+                                <td><form action=\"./PHP/taskCompleted.php\"><input class=\"txt3\" type=\"submit\"
                                   style=\"border-radius: 6px;\" type=\"submit\" value=\"Completed\"/></form></td></tr>";
                            }//while
                          }//else
