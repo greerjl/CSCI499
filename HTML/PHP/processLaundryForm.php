3595b839290f9c0e6d1032fb49a53831b91e854d
Caleb LaVergne <lavergcd@plu.edu>
2017-04-27 17:29:54 -0700

    User can now create a new event
    Event will have a name, be assigned to a room in the house, and have a date and time at which it will occur.

--- HTML/PHP/processLaundryForm.php
+++ HTML/PHP/processLaundryForm.php
@@ -1,50 +1,25 @@
 <?php
+session_start();
+include '../../../dbconnect.php';
+require_once("functions.php");
 ini_set("display_errors", true);
 error_reporting(E_ALL);
-	$eTitle = $eDesc = $username = $roomName = $sql = "";
-	$titleErr = $descErr = $roomNameErr = $uidErr = "";
-	$hasErrors = false;
+	$eTime = $eDate = $sql = "";
 
 	if($_SERVER['REQUEST_METHOD']=='POST' && $_POST){
-		$eTitle = cleanData($_POST['title']);
-			$titleErr = validate($eTitle, 'eTitle');
-			if(!empty($titleErr)){
-				$hasErrors = true;
-			}//if
-		$eDesc = cleadData($_POST['description']);
-			$descErr = validate($eDesc, 'eDesc');
-			if(!empty($descErr)){
-				$hasErrors = true;
-			}//if
-		$username = cleanData($_POST['username']);
-			$uidErr = validate($username, 'username');
-			if(!empty($uidErr)){
-				$hasErrors = true;
-			}//if
-
-		$roomName = $_POST['roomSelect'];
-			$roomNameErr = validate($roomName, 'roomName');
-			if(!empty($roomNameErr)){
-				$hasErrors = true;
-			}//if
+		$gid = $_SESSION["gid"];
+		$uid = $_SESSION["login_user"];
+				$eTime = $_POST['eventTime'];
+				$eDate = $_POST['eventDate'];
+				sendData($eTime, $eDate, $uid, $gid);
+				//redirect("../eventSettings.php");		
+			}
 
 	}//if
 
-	//FUNCTIONS
-	function cleanData($data){
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
-	}//cleanData
-
-	function validate($data, $field) {
-		switch($field) {
-			case 'eTitle':{
-				$data = strtolower($data);
-				$data = ucfirst($data);
-				$sql = "SELECT * FROM event WHERE name = '$data'"/* AND GID = '$gid'"*/;
-				$result = mysqli_query($db, $sql) /*or die("could not connect to DB")*/;
+	function validate($data, $gid) {
+				$sql = "SELECT * FROM event WHERE name = '$data' AND GID = '$gid'";
+				$result = mysqli_query($GLOBALS['db'], $sql);
 
 				$count = mysqli_num_rows($result);
 				if($count != 0){
@@ -53,64 +28,17 @@ error_reporting(E_ALL);
 				else {
 					return "";
 				}
-			}//case eTitle
-			case 'eDesc':{
-				if(strlen($data) > 140){
-					return "Description is too long.";
-				}
-				elseif(strlen($data) == 0){
-					return "Must have some description of the chore.";
-				}
-				else{
-					return "";
-				}
-			}//case eDesc
-			case 'username':{
-				$data = strtolower($data);
-				$sql = "SELECT UID FROM user_info WHERE username = '$data'"/* AND GID = '$gid'"*/;
-				$result = mysqli_query($db, $sql) /*or die("could not connect to DB")*/;
-
-				$count = mysqli_num_rows($result);
-				if($count == 0){
-					return "User is not in your group or incorrect username";
-				}
-				else{
-					return "";
-				}
-			}//case username
-
-			case 'roomName':{
-				if($data == ""){
-					return "Must select a room for your event.";
-				}//if
-				return "";
-			}//case roomName
-
-		}//switch
 	}//function validate
 
-	function sendData(){
-		if($_SERVER['REQUEST_METHOD']=='POST' && $_POST){
-				$sql = "SELECT RID FROM room WHERE name = '$roomName' AND GID = '$gid'";
-				$result = mysqli_query($db, $sql);
-
-				$count = mysqli_num_rows($result);
-				if($count != 1){
-					die('Error: ' . mysqli_error());
-				}
+	function sendData($eTitle, $eTime, $eDate, $roomID, $gid){
+				$datetime = date('Y-m-d H:i:s', strtotime("$eDate $eTime"));
+				$sql = "INSERT INTO event (time, RID, name, GID) VALUES ('$datetime','$roomID','$eTitle','$gid')";
+				$result = mysqli_query($GLOBALS['db'], $sql);
+				if(!$result){
+					die('Error: ' . mysqli_error($GLOBALS['db']));
+				}//if
 				else{
-					$rid = mysqli_fetch_row($result);
-					$rid = $rid[0];
-					$sql = "INSERT INTO event (RID, name, description, GID) VALUES ('$rid','$eTitle','$eDesc','$gid')";
-					$result = mysqli_query($db, $sql);
-
-					if(!$result){
-						die('Error: ' . mysqli_error());
-					}
-					else{
-						echo "Event successfully created!";
-					}//inner ifelse
-				}//outer ifelse
-		}//if
+					echo "Event successfully created!";
+				}//else
 	}//if
 ?>
