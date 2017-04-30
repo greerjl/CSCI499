35e14359a99e553f72dde2ad279734e22746a041
Gage <gibsongd@plu.edu>
2017-04-30 15:14:39 -0700

    banners
    scrapped the globals variable and moved the require_once function of the process login form into the request method if statement

--- HTML/login.php
+++ HTML/login.php
@@ -79,11 +79,10 @@ require_once("./PHP/functions.php");
       </div>
     <?php }//if
 
-    echo $GLOBALS['loginErr'];
 
-  //require_once('./PHP/processLoginForm.php');
   if($_SERVER["REQUEST_METHOD"] == "GET" || $hasErrors) {
-    if($GLOBALS['loginErr'] == true){ ?>
+    require_once('./PHP/processLoginForm.php');
+    if($loginErr == true){ ?>
       <div class="alert alert-danger">
         <strong>Error!</strong> User credentials are incorrect. Enter correct username and password.
       </div>
