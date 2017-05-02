02347b53e0783b3ed55a66b00ef5b927379462b4
Caleb LaVergne <lavergcd@plu.edu>
2017-05-01 18:01:08 -0700

    Implementing delete account
    Allows user to delete account. Will logout on success, or redirect with errors thrown if something goes wrong.

---
+++ HTML/PHP/deleteAccountForm.php
@@ -0,0 +1,25 @@
+<?php
+  session_start();
+  require("functions.php");
+  include "../../../dbconnect.php";
+
+  $uid = $_SESSION["login_user"];
+  $sql = "UPDATE user_info SET GID='0' WHERE UID='$uid';";
+  $result = mysqli_query($db, $sql);
+  if($result){
+    $_SESSION["gid"] = 0;
+    $sql = "DELETE FROM user_info WHERE UID = '$uid'";
+    $result = mysqli_query($db, $sql);
+    if($result){
+		logout();    
+    }//if
+    else{
+	    $_SESSION["deleteErr"] = 1;
+	    redirect("../userSettings.php");
+	 }//else
+  }//if
+  else{
+	    $_SESSION["deleteErr"] = 1;
+	    redirect("../userSettings.php");
+	}//else
+?>
