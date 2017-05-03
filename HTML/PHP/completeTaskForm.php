c532a1a2437bf7419290ed98a4d020b1f6c06812
Gage <gibsongd@plu.edu>
2017-05-02 23:06:09 -0700

    completeTask works
    stupid boolean qualifier was wrong

--- HTML/PHP/completeTaskForm.php
+++ HTML/PHP/completeTaskForm.php
@@ -1,25 +1,25 @@
 <?php
-	//ini_set("display_errors", true);
-	//error_reporting(E_ALL);
+	ini_set("display_errors", true);
+	error_reporting(E_ALL);
 	require_once("functions.php");
 	include '../../../dbconnect.php';
 
 	$emailErr = $dbResult = "";
 
 	if($_SERVER['REQUEST_METHOD']=='POST'){
+
 		/*USER CREDENTIALS*/
 		$taskname = mysqli_real_escape_string($db, $_POST['taskname']);
 
-
-		$sql = "SELECT TID FROM task WHERE name = '$col_value';"; //sql statement
+		$sql = "SELECT TID FROM task WHERE name = '$taskname';"; //sql statement
 		$result = mysqli_query($db, $sql); //query db
 		$count = mysqli_num_rows($result); //get number of rows in query
 		if($count == 1){
 			$query = mysqli_fetch_object($result); //get the task id
 			$tid = $query->TID;
-			$sql2 = "DELETE FROM task WHERE TID = '$col_value';"; //sql statement
+			$sql2 = "DELETE FROM task WHERE TID = '$tid';"; //sql statement
 			$result2 = mysqli_query($db, $sql2);
-			if($result2 != false){
+			if($result2 == false){
 				session_start();
 				$_SESSION["taskDeleteErr"] = 1;
 				redirect("../welcome.php");
