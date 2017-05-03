77e5e412b06294c7357c535260ffb12404cde6b1
Jayme Greer <greerjl@plu.edu>
2017-05-03 00:16:00 -0700

    no message

--- HTML/signup.php
+++ HTML/signup.php
@@ -146,11 +146,11 @@ require_once("./PHP/functions.php");
  </div><!--layout-->
 			 <!-- Footer -->
 		 <div id="footer">
-			 <div class="container">
+			 
 				 <p class="text-muted">Copyright ©2016-2017 PLU Capstone. House Utilities Manager.
 				 Authors <a target="_blank" href="https://www.linkedin.com/in/gagedgibson">Gage Gibson</a>,
 					 <a target="_blank" href="https://www.linkedin.com/in/jaymegreer">Jayme Greer</a> and Caleb LaVergne.</p>
-			 </div>
+
 		 </div>
 
 
