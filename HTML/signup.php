208ab9c179ab843ba2108da45902bc29a0fea9cb
Jayme Greer <greerjl@plu.edu>
2017-05-03 00:09:24 -0700

    no message

--- HTML/signup.php
+++ HTML/signup.php
@@ -153,28 +153,6 @@ require_once("./PHP/functions.php");
 			 </div>
 		 </div>
 
-		 <ul class="nav pull-right scroll-top">
-			 <li><a href="#" title="Scroll to top"><i class="glyphicon glyphicon-chevron-up"></i></a></li>
-		 </ul>
 
-		 <div class="modal" id="myModal" role="dialog">
-			 <div class="modal-dialog">
-				 <div class="modal-content">
-						<div class="modal-header">
-								<button class="close" type="button" data-dismiss="modal">×</button>
-								<h3 class="modal-title"></h3>
-						</div>
-						<div class="modal-body">
-							<div id="modalCarousel" class="carousel">
-								<a class="carousel-control left" href="#modaCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-								<a class="carousel-control right" href="#modalCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
-							</div>
-						</div>
-						<div class="modal-footer">
-								<button class="btn btn-default" data-dismiss="modal">Close</button>
-						</div>
-				 </div>
-		 </div>
-		</div> <!--footer-->
 	</body>
 </html>
